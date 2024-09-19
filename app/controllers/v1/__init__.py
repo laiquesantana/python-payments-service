05fd928b492b1731a56b6ce564918db064107d32
laique <laiquesantana@gmail.com>
2024-09-19 18:13:43 -0300

    fiz init

--- app/controllers/v1/__init__.py
+++ app/controllers/v1/__init__.py
@@ -1,7 +1,7 @@
 # Endpoint version: v1
 API_VERSION = "v1"  # pylint: disable=C0413
 
-from .payment_controller.payment_controller import router as payments_router_v1
+from .payment_controller.paypal_controller import router as payments_router_v1
 
 routes_v1 = [
     payments_router_v1,  # type: ignore
